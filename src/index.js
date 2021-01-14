1d867e5b79a9c9bed015fa03a31ad9997081a3f5
Oryam <48219965+or-yam@users.noreply.github.com>
2021-01-14 16:33:29 +0200

    moved state to redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import { searchRobots, requestRobots } from './reducers';
+import thunkMiddleware from 'redux-thunk';
+
+import 'tachyons';
+import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-import 'tachyons'; 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
